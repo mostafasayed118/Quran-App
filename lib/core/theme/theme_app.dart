135452354f1f4ea1c454341f9292db506dd29f78
Mustafa Al3tar <mustafa_620125@oi.edu.eg>
2024-08-23 00:24:40 +0300

    Push Project

---
+++ lib/core/theme/theme_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  useMaterial3: true,
+);
+
+ThemeData darkMode= ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  useMaterial3: true,
+);
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:quran_app/app/app.dart';
+
+void main() {
+  runApp(const QuranApp());
+}
